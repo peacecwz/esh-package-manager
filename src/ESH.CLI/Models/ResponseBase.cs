abaab3c605db69839eafeaf64f08efb193d2e2c4
Barış Ceviz <me@barisceviz.com>
2018-09-08 01:42:07 +0300

    Cmmited project

---
+++ src/ESH.CLI/Models/ResponseBase.cs
@@ -0,0 +1,9 @@
+﻿namespace ESH.CLI.Models
+{
+    public class ResponseBase<T> where T : class
+    {
+        public bool IsSucess { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ src/ESH.CLI/Options/ESHConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ESH.CLI.Options
+{
+    public class ESHConfig
+    {
+        public string Name { get; set; }
+        public List<ESHDependency> Dependencies { get; set; }
+    }
+
+    public class ESHDependency
+    {
+        public string Name { get; set; }
+        public string GitRepository { get; set; }
+        public string LastModifiedOn { get; set; }
+    }
+}
