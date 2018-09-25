93352f4d61694c066ee9bf2ebafb07910a7a9984
Baris Ceviz <me@barisceviz.com>
2018-09-26 00:16:41 +0300

    Initialized configuration for package notes and more

---
+++ src/ESH.CLI/Common/ESHConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ESH.CLI.Common
+{
+    public class EshConfig
+    {
+        public string Name { get; set; }
+        public List<EshDependency> Dependencies { get; set; }
+    }
+}
---
+++ src/ESH.CLI/Common/EshDependency.cs
@@ -0,0 +1,9 @@
+﻿namespace ESH.CLI.Common
+{
+    public class EshDependency
+    {
+        public string Name { get; set; }
+        public string GitRepository { get; set; }
+        public string LastModifiedOn { get; set; }
+    }
+}
