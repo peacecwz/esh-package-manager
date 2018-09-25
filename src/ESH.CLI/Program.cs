704e7f6b64caabafecd4e44a34bc200b5a39edbf
Baris Ceviz <me@barisceviz.com>
2018-09-26 00:15:53 +0300

    Created project

--- src/ESH.CLI/Program.cs
+++ src/ESH.CLI/Program.cs
@@ -1,31 +1,21 @@
-﻿using ESH.CLI.Services;
-using Microsoft.Extensions.CommandLineUtils;
-using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ESH.CLI
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            var app = new CommandLineApplication()
-            {
-                Name = "esh",
-                Description =
-                    "ESH Package Manager is managing your packages with git repository and working without nuget"
-            };
-            var services = new EshServices();
-            app.HelpOption("-h|--help");
-            app.Command("install", services.InstallCommand);
-            app.Command("add", services.AddCommand);
-            app.Command("update", services.UpdateCommand);
-            app.Command("remove", services.RemoveCommand);
-            app.Command("list", services.ListCommand);
+            var services = new ServiceCollection()
+                .AddSingleton<IConsole>(PhysicalConsole.Singleton)
+                .BuildServiceProvider();
 
-            app.OnExecute(() => 0);
-
-            app.Execute(args);
-            Console.ReadKey();
+            var app = new CommandLineApplication<App>();
+            app.Conventions
+                .UseDefaultConventions()
+                .UseConstructorInjection(services);
+            return app.Execute(args);
         }
     }
 }
