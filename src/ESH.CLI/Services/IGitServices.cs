3afc5bd1a8cd5dbadfe143124cd32ca06ab3330e
Baris Ceviz <me@barisceviz.com>
2018-09-26 00:17:24 +0300

    Implemented Git repository services

---
+++ src/ESH.CLI/Services/IGitServices.cs
@@ -0,0 +1,12 @@
+﻿using ESH.CLI.Models;
+using LibGit2Sharp;
+
+namespace ESH.CLI.Services
+{
+    public interface IGitServices
+    {
+        void Update(string repositoryName);
+        void UpdateAll();
+        ResponseBase<Repository> Clone(string gitRepositoryUrl);
+    }
+}
