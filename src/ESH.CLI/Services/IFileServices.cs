78b45be2ff70a117bffa84d944f0c8efb205c28a
Baris Ceviz <me@barisceviz.com>
2018-09-26 00:17:12 +0300

    Implemented file services

---
+++ src/ESH.CLI/Services/IFileServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ESH.CLI.Common;
+
+namespace ESH.CLI.Services
+{
+    public interface IFileServices
+    {
+        string GetPackageFolder(Uri gitRepo);
+        string GetPackageFolderByRepositoryName(string repositoryName);
+        string GetGitRepositoryName(Uri gitRepo);
+        void CreatePackagesFolder();
+        EshConfig GetConfig();
+    }
+}
---
+++ src/ESH.CLI/Services/Implementations/FileServices.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ESH.CLI.Common;
+using Newtonsoft.Json;
+
+namespace ESH.CLI.Services.Implementations
+{
+    public class FileServices : IFileServices
+    {
+        public void CreatePackagesFolder()
+        {
+            if (!Directory.Exists("esh_packages"))
+            {
+                Directory.CreateDirectory("esh_packages");
+            }
+        }
+
+        public string GetPackageFolder(Uri gitRepo)
+        {
+            return $"esh_packages/{GetGitRepositoryName(gitRepo)}";
+        }
+
+        public string GetPackageFolderByRepositoryName(string repositoryName)
+        {
+            return $"esh_packages/{repositoryName}";
+        }
+
+        public string GetGitRepositoryName(Uri gitRepo)
+        {
+            return gitRepo.LocalPath.Split('/')?.LastOrDefault()?.Replace(".git", "");
+        }
+
+        public EshConfig GetConfig()
+        {
+            if (!File.Exists("esh.json"))
+            {
+                File.WriteAllText("esh.json", "{}", Encoding.UTF8);
+            }
+
+            string json = File.ReadAllText("esh.json");
+
+            return JsonConvert.DeserializeObject<EshConfig>(json);
+        }
+    }
+}
